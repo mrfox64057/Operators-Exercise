180bd2c1b4baffc4159020465013361055572856
Adam Jankowski <ghost17814@gmail.com>
2023-04-11 13:49:59 -0400

    final commit

---
+++ OperatorExercise/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorExercise
+{
+    internal class Class1
+    {
+        public static double AreaOfCircle(double radius)
+        {
+            var area = Math.PI * radius * radius;
+            return area;
+        }
+
+    }
+}
--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -10,8 +10,14 @@
             int remainder = a % b;
             if (a == 17 && b == 4) ;
             Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
-
-                
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+            Console.WriteLine(AreaOfCircle(radius));
+        }
+        public static double AreaOfCircle(double radius)
+        {
+            var area = Math.PI * radius * radius;
+            return area;
         }
     }
 }
