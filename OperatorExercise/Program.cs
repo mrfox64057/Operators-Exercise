0d499d7e4b962eab982dab83fba654d1586aab08
Adam Jankowski <ghost17814@gmail.com>
2023-04-11 15:01:43 -0400

    The actual final commit for real this time

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,15 +4,21 @@
     {
         static void Main(string[] args)
         {
-            int a = 17;
-            int b = 4;
-            int quotient = a / b;
-            int remainder = a % b;
+            var a = 17;
+            var b = 4;
+            var sum = a + b;
+            var sum2 = a - b;
+            var quotient = a / b;
+            var remainder = a % b;
             if (a == 17 && b == 4) ;
+            Console.WriteLine($"{a}+{b}={sum}");
+            Console.WriteLine($"{a}-{b}={sum2}");
+            Console.WriteLine($"{a}/{b}={quotient}");
+            Console.WriteLine($"The remainder of {a}/{b} is {remainder}");
             Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
             Console.WriteLine("What is the radius of your circle?");
             var radius = double.Parse(Console.ReadLine());
-            Console.WriteLine(AreaOfCircle(radius));
+            Console.WriteLine(Class1.AreaOfCircle(radius));
         }
         public static double AreaOfCircle(double radius)
         {
